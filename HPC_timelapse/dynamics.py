77b96c492dc4dd7eb681d7556128d21f3c1b4124
Akash Patil <akash2320@gmail.com>
2025-01-10 12:09:36 -0500

    updating

--- HPC_timelapse/dynamics.py
+++ HPC_timelapse/dynamics.py
@@ -3,76 +3,37 @@ import os
 import sys
 from tqdm import tqdm
 import pickle
-#from scipy import ndimage as scp
+from scipy import ndimage as scp
 from statsmodels.tsa.stattools import acf
 from natsort import natsorted
 import cv2
-import multiprocessing
+# import multiprocessing
 from numpy.fft import fft2,fft,ifft
 # from itertools import repeat
-from utils.py import *
+from utils import *
 
 os.chdir('../../../../../../../../N/project/OCT_preproc/CELL_DYNAMICS/')
-
-def load_data(path):
-    pic_paths = []
-    for scan_num in os.listdir(path):
-        pic_paths.append(os.path.join(path,scan_num,f'{scan_num}.pickle'))
-    pic_paths = natsorted(pic_paths)
-
-    with open(f'{pic_paths[0]}', 'rb') as handle:
-        b = pickle.load(handle)
-    data = np.zeros((len(pic_paths),b.shape[0],b.shape[1],b.shape[2]))
-
-    for idx,img_path in enumerate(pic_paths):
-        temp = pickle.load(img_path)
-        data[i]=(temp.copy())
-    data = data.astype(np.float32)
-    return data
-
-
-def slope_mask_10batch(slope_arr):
-    mask1 = np.zeros_like(slope_arr[0],dtype=np.float32)
-    # slope_arr = slope_arr.astype(np.float32)
-    std_mask = np.apply_along_axis(func1d=np.std,arr=slope_arr,axis=0)
-    slope_arr = np.apply_along_axis(func1d=min_max,arr=slope_arr,axis=0)
-    for x in range(slope_arr.shape[1]):
-        for y in range(slope_arr.shape[2]):
-            data1 = slope_arr[:,x,y]
-            slope1 = np.polyfit(range(len(data1)), data1, 1)[0]
-            mask1[x, y] = -np.abs(slope1)
-    return mask1*(5*std_mask)
-
-def ymotion(data):
-    n = data.shape[0]
-    data[0,0,250:500,:]
-    nn = [np.argmax(np.sum(data[i][0,250:500,:],axis=1)) for i in range(data.shape[0])]
-    tf_all_nn = np.tile(np.eye(3),(data.shape[0],1,1))
-    for i in range(tf_all_nn.shape[0]):
-        tf_all_nn[i] = np.dot(tf_all_nn[i],AffineTransform(translation=(0,-(nn[0]-nn[i]))))
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            data[i][j]  = warp(data[i][j],AffineTransform(matrix=tf_all_nn[i]),order=3)
-    return data
-
-# def image(x_idx_batch, X_shape, shared_array):
-#     data1 = shared_array.astype(np.float32)
-#     # mask_temp = autocorr(data1)
-#     # mask_temp = corr_fft(data1)
-#     mask_temp = fft_slope(data1)
-#     # mask_temp = slope_mask_10batch(data1)
-#     return mask_temp , x_idx_batch
 
 
 if __name__ == '__main__':
     # num = int(sys.argv[1])
     path = 'timelapse_withH2O2_registered/registered_pickles/'
+    # path = '/Users/akapatil/Documents/OCT/timelapse/Timelapse_with_H2O2_12_20_2024/registered_pickles/'
+    print('LOADING DATA')
     data = load_data(path)
+    print('DATA LOADED')
+
+    print("SHAPE: ",data.shape)
+
+    print('Y-MOTION')
     data = ymotion(data)
+    print('Y-MOTION CORRECTED')
 
     masks = np.zeros((data.shape[1],data.shape[2],data.shape[3]), dtype=np.float32)
     for i in range(masks.shape[0]):
-        masks[i] = slope_mask_10batch(np.squeeze(data[:,i,:,:]))
+        masks[i] = slope_mask(np.squeeze(data[:,i,:,:]))
+
+    print('MASKS GENERATED')
 
     os.makedirs('masks',exist_ok=True)
     for idx, img in enumerate(masks):
@@ -116,6 +77,6 @@ if __name__ == '__main__':
     #     mask_each_batch[x_idx] = m
 
 
-    # with open(f'tmp/{data_name}_mask_{batch}.pickle', 'wb') as handle:
-    #     pickle.dump(mask_each_batch, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(f'mask.pickle', 'wb') as handle:
+        pickle.dump(masks, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
--- HPC_timelapse/utils.py
+++ HPC_timelapse/utils.py
@@ -10,11 +10,50 @@ import cv2
 import multiprocessing
 from numpy.fft import fft2,fft,ifft
 
-
-
 def min_max(data1):
     if np.max(data1)==0:
         return data1
     else:
         data1 = (data1-np.min(data1))/(np.max(data1)-np.min(data1))
         return data1
+
+def load_data(path):
+    pic_paths = []
+    for scan_num in os.listdir(path):
+        if scan_num.startswith('scan'):
+            pic_paths.append(os.path.join(path,scan_num,f'{scan_num}.pickle'))
+    pic_paths = natsorted(pic_paths)
+    with open(f'{pic_paths[0]}', 'rb') as handle:
+        b = pickle.load(handle)
+    data = np.zeros((len(pic_paths),b.shape[0],b.shape[1],b.shape[2]))
+
+    for idx,img_path in enumerate(pic_paths):
+        with open(img_path, 'rb') as handle:
+            temp = pickle.load(handle)
+        data[idx]=(temp.copy())
+    data = data.astype(np.float32)
+    return data
+
+def slope_mask(slope_arr):
+    mask1 = np.zeros_like(slope_arr[0],dtype=np.float32)
+    # slope_arr = slope_arr.astype(np.float32)
+    std_mask = np.apply_along_axis(func1d=np.std,arr=slope_arr,axis=0)
+    slope_arr = np.apply_along_axis(func1d=min_max,arr=slope_arr,axis=0)
+    for x in range(slope_arr.shape[1]):
+        for y in range(slope_arr.shape[2]):
+            data1 = slope_arr[:,x,y]
+            slope1 = np.polyfit(range(len(data1)), data1, 1)[0]
+            mask1[x, y] = -np.abs(slope1)
+    return mask1*(5*std_mask)
+
+def ymotion(data):
+    n = data.shape[0]
+    data[0,0,250:500,:]
+    nn = [np.argmax(np.sum(data[i][0,250:500,:],axis=1)) for i in range(data.shape[0])]
+    tf_all_nn = np.tile(np.eye(3),(data.shape[0],1,1))
+    for i in range(tf_all_nn.shape[0]):
+        tf_all_nn[i] = np.dot(tf_all_nn[i],AffineTransform(translation=(0,-(nn[0]-nn[i]))))
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            data[i][j]  = warp(data[i][j],AffineTransform(matrix=tf_all_nn[i]),order=3)
+    return data
